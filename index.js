6431a1e8296df940bf58278be369de12c7a5d58a
Lucif3r-in <ashutosh123rath@gmail.com>
2023-07-24 13:44:09 +0530

    feat: add mongoose setup & add packages

---
+++ index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+
+const CONNECTION_URL = process.env.CONNECTION_URL;
+const PORT = process.env.PORT || 5000;
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() =>
+    app.listen(PORT, () =>
+      console.log(`Server Running on Port: http://localhost:${PORT}`)
+    )
+  )
+  .catch((error) => console.log(`${error} did not connect`));
+
+mongoose.set("useFindAndModify", false);
